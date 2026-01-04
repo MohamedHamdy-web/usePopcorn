1f941da31ae46a3b72ec5912d02724b2ae5ab71e
hamdy <mou.sabbah@gmail.com>
2026-01-04 06:57:07 +0200

    Finalize usePopcorn app

--- src/App.js
+++ src/App.js
@@ -146,27 +146,6 @@ function Box({ children }) {
   );
 }
 
-// function WatchedBox() {
-//   const [isOpen2, setIsOpen2] = useState(true);
-
-//   return (
-//     <div className="box">
-//       <button
-//         className="btn-toggle"
-//         onClick={() => setIsOpen2((open) => !open)}
-//       >
-//         {isOpen2 ? "–" : "+"}
-//       </button>
-//       {isOpen2 && (
-//         <>
-//           <WatchedSummary watched={watched} />
-//           <WatchedMovieList watched={watched} />
-//         </>
-//       )}
-//     </div>
-//   );
-// }
-
 function MoiveList({ movies, onSelectMovie }) {
   return (
     <ul className="list list-movies">
